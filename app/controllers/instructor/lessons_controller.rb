8b0b49c33e7065038716135a5b3968ff49762dce
Gabriel Romero <GPRomero221@gmail.com>
2019-12-22 21:11:45 +0000

    video uploading enabled

--- app/controllers/instructor/lessons_controller.rb
+++ app/controllers/instructor/lessons_controller.rb
@@ -25,6 +25,6 @@ helper_method :current_section
   end
 
   def lesson_params
-    params.require(:lesson).permit(:title, :subtitle)
+    params.require(:lesson).permit(:title, :subtitle, :video)
   end
 end
